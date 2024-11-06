c75d312477f93656c2433893fd5e8c273999fda7
ManahilHabibb <manahil5503@gmail.com>
2024-11-06 23:53:39 +0500

    updated

--- components/button.jsx
+++ components/button.jsx
@@ -1,18 +1,37 @@
 import React from "react";
-import { Text, View , StyleSheet} from "react-native";
+import { Text, View , StyleSheet , TouchableOpacity} from "react-native";
 
-const Button = ({  }) => {
+const Button = ({ title }) => {
     return (
         <View style={styles.container}>
-          
-                
+            
+            <TouchableOpacity style={styles.button} onPress={() => { }}>
+                <Text style={styles.buttonText}>
+                    {title}
+                </Text>
+                </TouchableOpacity>
            
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-   
+   button: {
+       alignItems: "center",
+       justifyContent: "center",
+       paddingVertical: 12,
+       paddingHorizontal: 32,
+       borderRadius: 4,
+       elevation: 3,
+       backgroundColor: "#00c395",
+   },
+   buttonText: {
+       fontSize: 16,
+       lineHeight: 21,
+       fontWeight: "bold",
+       letterSpacing: 0.25,
+       color: "white",
+   },
 });
 
 export default Button;
--- components/longbutton.jsx
+++ components/longbutton.jsx
@@ -1,18 +1,38 @@
 import React from "react";
 import { Text, View , StyleSheet} from "react-native";
 
-const LongButton = ({  }) => {
+const LongButton = ({ title }) => {
     return (
         <View style={styles.container}>
-          
-                
+            
+            <TouchableOpacity style={styles.button} onPress={() => { }}>
+                <Text style={styles.buttonText}>
+                    {title}
+                </Text>
+                </TouchableOpacity>
            
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-   
+   button: {
+       alignItems: "center",
+       justifyContent: "center",
+       paddingVertical: 12,
+       paddingHorizontal: 32,
+       borderRadius: 4,
+       elevation: 3,
+       backgroundColor: "#00c395",
+   },
+   buttonText: {
+       fontSize: 16,
+       lineHeight: 21,
+       fontWeight: "bold",
+       letterSpacing: 0.25,
+       color: "white",
+   },
 });
+
 
 export default LongButton;
